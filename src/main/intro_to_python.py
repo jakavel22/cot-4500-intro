d182299a8a790b733bb221f47bbe8f9ed52c8915
Janny Velazquez <122475502+jakavel22@users.noreply.github.com>
2023-01-21 16:42:45 -0500

    Update intro_to_python.py

--- src/main/intro_to_python.py
+++ src/main/intro_to_python.py
@@ -12,11 +12,13 @@ def Intro():
     #Print a specific 3x3 matrix where a cell is 1 if i == j, else 0 
     matrix = np.identity(3)
     print(matrix)
+    print('')
 
 
     #Print the 3x3 matrix from #1 and then add 3 to every cell where i ≠j  
     matrix2=np.where(matrix !=0, matrix, matrix+3)
     print(matrix2)
+    print('')
 
 
     #Print the 3x3 matrix from #2 as a 3x2 by deleting the last column from the matrix created
